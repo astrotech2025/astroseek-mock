d3175ef54d1cec6931dd23802220b3120c9c7e7b
astrotech2025 <massimilianodeflorio@gmail.com>
2025-04-23 08:48:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/astroseek", methods=["GET"])
+def get_mock_data():
+    nome = request.args.get("nome")
+    return jsonify({
+        "ascendente": "Vergine",
+        "luna": "Scorpione",
+        "sole": "Acquario",
+        "nome": nome
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
