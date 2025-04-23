602b2903e8156e6db6af178b4705e23a1c38c947
astrotech2025 <massimilianodeflorio@gmail.com>
2025-04-23 09:31:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Abilita i CORS per tutte le origini
 
 @app.route("/astroseek", methods=["GET"])
 def get_mock_data():
